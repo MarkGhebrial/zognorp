59082610c35b5d89888368fce3311aae76779909
MarkGhebrial <mghebrialjr@icloud.com>
2026-08-01 17:54:48 -0700

    Add some extra puzzles to the main function

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,5 @@
+use std::collections::HashSet;
+
 use crate::{
     puzzle::{Cell, Puzzle},
     solver::solve_sudoku,
@@ -8,9 +10,8 @@ mod solver;
 mod sort;
 
 fn main() {
-    let mut grid: [Cell; 81] = [Cell::Unset; 81];
-
     #[rustfmt::skip]
+    let mut grid: [Cell; 81] = 
     [
         5, 3, 0, 0, 7, 0, 0, 0, 0,
         6, 0, 0, 1, 9, 5, 0, 0, 0,
@@ -22,17 +23,62 @@ fn main() {
         0, 0, 0, 4, 1, 9, 0, 0, 5,
         0, 0, 0, 0, 8, 0, 0, 7, 9 
     ]
+        .map(|c: u8| Cell::from(c));
+
+    let puzzle = Puzzle::new(grid);
+
+    match solve_sudoku(puzzle) {
+        Ok(p) => println!("Found a solution for the first puzzle!\n{p}"),
+        Err(e) => println!("{}", e),
+    }
+
+    #[rustfmt::skip]
+    [
+        2, 0, 0, 0, 1, 0, 3, 6, 0,
+        1, 0, 0, 9, 0, 0, 5, 0, 0,
+        0, 0, 3, 0, 0, 4, 0, 0, 0,
+        0, 1, 0, 3, 7, 0, 0, 0, 0,
+        7, 0, 0, 0, 0, 0, 0, 0, 6,
+        0, 5, 4, 1, 0, 0, 0, 0, 0,
+        0, 0, 7, 0, 0, 3, 0, 5, 0,
+        9, 0, 0, 0, 6, 0, 0, 0, 0,
+        0, 8, 0, 0, 4, 9, 0, 0, 0,
+    ]
         .iter()
         .map(|c| Cell::from(*c))
         .enumerate()
         .for_each(|(i, c)| grid[i] = c);
 
-    println!("67: {:?}", grid[67]);
-
     let puzzle = Puzzle::new(grid);
 
-    match solve_sudoku(puzzle) {
-        Ok(p) => println!("Found a solution!"),
+    match solve_sudoku(puzzle, &mut HashSet::new()) {
+        Ok(p) => println!("Found a solution for the second puzzle!\n{p}"),
         Err(e) => println!("{}", e),
     }
+
+    // let mut grid: [Cell; 81] = [Cell::Unset; 81];
+
+    // #[rustfmt::skip]
+    // [
+    //     0, 0, 0, 2, 0, 0, 6, 5, 0,
+    //     7, 0, 0, 0, 0, 0, 0, 0, 0,
+    //     0, 6, 0, 0, 0, 9, 0, 0, 0,
+    //     6, 1, 0, 7, 0, 0, 0, 9, 0,
+    //     0, 0, 7, 0, 1, 0, 0, 0, 5,
+    //     4, 0, 0, 0, 0, 5, 0, 0, 0,
+    //     3, 0, 0, 0, 0, 0, 1, 0, 0,
+    //     0, 0, 0, 9, 2, 8, 0, 0, 4,
+    //     0, 0, 0, 0, 0, 0, 0, 2, 8
+    // ]
+    //     .iter()
+    //     .map(|c| Cell::from(*c))
+    //     .enumerate()
+    //     .for_each(|(i, c)| grid[i] = c);
+
+    // let puzzle = Puzzle::new(grid);
+
+    // match solve_sudoku(puzzle, &mut HashSet::new()) {
+    //     Ok(p) => println!("Found a solution!\n{p}"),
+    //     Err(e) => println!("{}", e),
+    // }
 }
