979829d8064ac2ddde4e4a20ad9c572e99c35330
MarkGhebrial <mghebrialjr@icloud.com>
2026-08-02 19:57:20 -0700

    Remove unneeded allocation (things are _very_ fast now)

--- src/solver.rs
+++ src/solver.rs
@@ -12,26 +12,22 @@ pub fn solve_sudoku(grid: &Grid) -> Option<Grid> {
         return Some(grid.clone());
     }
 
-    let all_possibilities: Vec<(usize, &CellPossibilities)> = grid
+    // Find the unset cell with the fewest amount of possibilities
+    let (best_cell_index, best_cell_possibilities): (usize, &CellPossibilities) = grid
         .possibilities()
         .iter()
         .enumerate()
         .filter(|(cell_index, _)| grid.cells()[*cell_index] == 0) // Filter out cells that've already been set
-        .collect();
-
-    // Find the cell with the fewest amount of possibilities
-    let (best_cell_index, best_cell_possibilities) = all_possibilities
-        .iter()
         .min_by_key(|(_, possibilities)| possibilities.count())
         .unwrap();
 
-    // Try every number possible value for the cell with the fewest possibilities
+    // Try every possible value for the cell with the fewest possibilities
     for cell_value in (1..=9)
         .map(|n: u8| UBoundU8::<9>::new(n))
         .filter(|n| best_cell_possibilities.is_possible(*n))
     {
         let mut new_puzzle = grid.clone();
-        new_puzzle.set_cell(BoundedU8::new(*best_cell_index as u8), cell_value);
+        new_puzzle.set_cell(BoundedU8::new(best_cell_index as u8), cell_value);
 
         // Recursively solve the new puzzle
         match solve_sudoku(&new_puzzle) {
