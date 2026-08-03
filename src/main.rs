0a4ebca3ed690a9d3a010208a1b123b2e9c5a2a7
MarkGhebrial <mghebrialjr@icloud.com>
2026-08-02 18:13:13 -0700

    Load puzzles from puzzle.txt

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,5 @@
+use std::time::{Duration, Instant};
+
 use crate::{bounded_u8::UBoundU8, grid::Grid, solver::solve_sudoku};
 
 mod bounded_u8;
@@ -6,88 +8,56 @@ mod puzzle;
 mod solver;
 mod sort;
 
+fn load_puzzles() -> Vec<[UBoundU8<9>; 81]> {
+    let file_contents = std::fs::read_to_string("puzzles.txt").unwrap();
+
+    file_contents
+        .lines()
+        .filter(|line| !line.is_empty() && !line.starts_with("#"))
+        .map(|line| {
+            let mut cells: [UBoundU8<9>; 81] = [UBoundU8::new(0); 81];
+
+            if line.len() != 81 {
+                panic!(
+                    "Error loading puzzle. Line had {} bytes, expected 81",
+                    line.len()
+                );
+            }
+
+            for (i, byte) in line.bytes().enumerate() {
+                if (b'0'..=b'9').contains(&byte) {
+                    cells[i] = UBoundU8::new(byte - b'0');
+                }
+            }
+
+            cells
+        })
+        .collect()
+}
+
 fn main() {
-    #[rustfmt::skip]
-    let grid: [UBoundU8<9>; 81] =
-    [
-        5, 3, 0, 0, 7, 0, 0, 0, 0,
-        6, 0, 0, 1, 9, 5, 0, 0, 0,
-        0, 9, 8, 0, 0, 0, 0, 6, 0,
-        8, 0, 0, 0, 6, 0, 0, 0, 3,
-        4, 0, 0, 8, 0, 3, 0, 0, 1,
-        7, 0, 0, 0, 2, 0, 0, 0, 6,
-        0, 6, 0, 0, 0, 0, 2, 8, 0,
-        0, 0, 0, 4, 1, 9, 0, 0, 5,
-        0, 0, 0, 0, 8, 0, 0, 7, 9 
-    ]
-        .map(|c: u8| UBoundU8::new(c));
+    let mut total_solve_time = Duration::ZERO;
+    let mut num_puzzles = 0;
 
-    let grid = Grid::new(grid);
+    for (i, puzzle) in load_puzzles().into_iter().enumerate() {
+        let start = Instant::now();
+        let grid = Grid::new(puzzle);
+        let solution = solve_sudoku(&grid);
+        let elapsed = start.elapsed();
 
-    // println!("{}", grid);
+        total_solve_time += elapsed;
+        num_puzzles += 1;
 
-    // for (cell_index, possibilities) in grid.possibilities().iter().enumerate() {
-    //     // Skip set cells
-    //     if *grid.cells()[cell_index] != 0 {
-    //         continue;
-    //     }
-
-    //     print!("Possibilities for cell {}: ", cell_index);
-    //     for i in 1..9 {
-    //         if possibilities.is_possible(BoundedU8::new(i)) {
-    //             print!("{i}, ");
-    //         }
-    //     }
-    //     println!();
-    // }
-
-    match solve_sudoku(&grid) {
-        Ok(p) => println!("Found a solution for the first puzzle!\n{p}"),
-        Err(e) => println!("{}", e),
+        match solution {
+            Ok(_grid) => println!("Solved puzzle {} in {}us", i, elapsed.as_micros()),
+            Err(err) => println!("Failed to solve puzzle {}: {}", i, err),
+        }
     }
 
-    #[rustfmt::skip]
-    let grid: [UBoundU8<9>; 81] =
-    [
-        2, 0, 0, 0, 1, 0, 3, 6, 0,
-        1, 0, 0, 9, 0, 0, 5, 0, 0,
-        0, 0, 3, 0, 0, 4, 0, 0, 0,
-        0, 1, 0, 3, 7, 0, 0, 0, 0,
-        7, 0, 0, 0, 0, 0, 0, 0, 6,
-        0, 5, 4, 1, 0, 0, 0, 0, 0,
-        0, 0, 7, 0, 0, 3, 0, 5, 0,
-        9, 0, 0, 0, 6, 0, 0, 0, 0,
-        0, 8, 0, 0, 4, 9, 0, 0, 0,
-    ]
-        .map(|c: u8| UBoundU8::new(c));
-
-    let grid = Grid::new(grid);
-    match solve_sudoku(&grid) {
-        Ok(p) => println!("Found a solution for the second puzzle!\n{p}"),
-        Err(e) => println!("{}", e),
-    }
-
-    // let mut grid: [Cell; 81] = [Cell::Unset; 81];
-
-    #[rustfmt::skip]
-    let grid: [UBoundU8<9>; 81] =
-    [
-        0, 0, 0, 2, 0, 0, 6, 5, 0,
-        7, 0, 0, 0, 0, 0, 0, 0, 0,
-        0, 6, 0, 0, 0, 9, 0, 0, 0,
-        6, 1, 0, 7, 0, 0, 0, 9, 0,
-        0, 0, 7, 0, 1, 0, 0, 0, 5,
-        4, 0, 0, 0, 0, 5, 0, 0, 0,
-        3, 0, 0, 0, 0, 0, 1, 0, 0,
-        0, 0, 0, 9, 2, 8, 0, 0, 4,
-        0, 0, 0, 0, 0, 0, 0, 2, 8
-    ]
-        .map(|c: u8| UBoundU8::new(c));
-
-    let grid = Grid::new(grid);
-
-    match solve_sudoku(&grid) {
-        Ok(p) => println!("Found a solution to the third puzzle!\n{p}"),
-        Err(e) => println!("{}", e),
-    }
+    println!(
+        "Solved {} puzzles in {}s (average: {}ms)",
+        num_puzzles,
+        total_solve_time.as_secs_f64(),
+        (total_solve_time / num_puzzles).as_micros() as f64 / 1000.0
+    );
 }
